6e7871920aa32523e49401b53a360bfa393e09bd
Fredrik93 <fredrikullman93@gmail.com>
2026-06-19 07:52:12 +0200

    factorial

--- src/main/scala/Recursion.scala
+++ src/main/scala/Recursion.scala
@@ -8,11 +8,15 @@ def tailRecursiveFactorial (n: Int, acc: Int = 1): Int =
         println(n)
         tailRecursiveFactorial(n-1, acc * n)
          
-// this will fail since it is not tail recursive and the annotation is checking whether it is. Its warning us because if it is not tail recursive it will throw an exception
-// stackoverflow error whenever too many frames are pushed to the stack. tail recursive method reuses the same frame instead of stacking thousands on top of each other
+/*
+The method will fail since it is not tail recursive and the annotation is checking whether it is. 
+Its warning us because if it is not tail recursive it will throw an exception
+stackoverflow error whenever too many frames are pushed to the stack. 
+tail recursive method reuses the same frame instead of stacking thousands on top of each other
+*/ 
 @tailrec
 def recursiveFactorial (n: Int): Int = 
     if n <= 1 then 1
     else 
         println(n)
-        recursiveFactorial(n-1)
+        n * recursiveFactorial(n-1)
