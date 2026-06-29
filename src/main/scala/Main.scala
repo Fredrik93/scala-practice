b5be48438c905edd20459ed68a899f05ff5b5ce3
Fredrik93 <fredrikullman93@gmail.com>
2026-06-29 06:52:20 +0200

    animals

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,7 @@
 @main def theMainiestOfMainMethods(): Unit =
   val trains = List(ShortTrain, LongTrain, Commuter, TGV("tgv"))
 
-  val speeds = trains.map(_.maxSpeed)
-
-  val summedMaxSpeed = trains.map(n=> n.maxSpeed).sum
-  println(s"sum of max speed: ${summedMaxSpeed}")
-  println(speeds)
+  val a1 = FurAnimal(3)
+  val a2 = AnAnimal(0)
+  println(a1.sound)
+  println(a2.sound)
--- src/main/scala/Train.scala
+++ src/main/scala/Train.scala
@@ -1,19 +1,30 @@
 trait TrainFeatures:
-  val maxSpeed: Int
+  val maxSpeed: Int = 50
   def describe: String
 
 case object LongTrain extends TrainFeatures:
-  val maxSpeed = 120
+  override val maxSpeed = 120
   def describe: String = "A long train"
 
 case object ShortTrain extends TrainFeatures:
-  val maxSpeed = 200
+  override val maxSpeed = 200
   def describe: String = "A short train"
 
 case object Commuter extends TrainFeatures:
-  val maxSpeed = 80
+  override val maxSpeed = 80
   def describe: String = "A commuter train"
 
 case class TGV(typeOfTrain: String) extends TrainFeatures:
-  val maxSpeed = 190
   def describe: String = s"The ${typeOfTrain} train"
+
+trait Animal(name: String)
+
+abstract class OtherAnimal (val name: String):
+  def sound: String
+  
+case class FurAnimal(weight: Int) extends OtherAnimal("cow"):
+  def sound: String = "A sound from an animal"
+
+
+case class AnAnimal(weight: Int) extends Animal("snake"):
+  def sound: String = "A sound from a snake"
