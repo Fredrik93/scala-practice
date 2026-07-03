65ed6ac2380801a1e39b61c438d9ae8813d32e1b
Fredrik93 <fredrikullman93@gmail.com>
2026-07-03 11:52:13 +0200

    update

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,4 @@
 import functional.CalcNumbers
 
 @main def theMainiestOfMainMethods(): Unit =
-  println(CalcNumbers.squared(23))
-  println(CalcNumbers.isEmpty(""))
-  println(CalcNumbers.isEmpty("helloo"))
-  println(CalcNumbers.concatenate("micho", "macho"))
+  println(CalcNumbers.applyToAll(List(1,2,3,4)))
--- src/main/scala/functional/CalcNumbers.scala
+++ src/main/scala/functional/CalcNumbers.scala
@@ -5,5 +5,9 @@ object CalcNumbers:
   val isEmpty: String => Boolean = (s: String) => s.isEmpty
   val concatenate: (String, String) => String = (s: String, s2: String) => s + " " + s2
 
+  def triple(x: Int): Int = x * 3
+  val tripleVal: Int => Int = x => x * 3
+
+  def applyToAll(list: List[Int]): Int = -12
 
 
